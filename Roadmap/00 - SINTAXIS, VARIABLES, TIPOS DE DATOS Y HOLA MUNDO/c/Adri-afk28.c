610d6843ce3af8b52031f6eaaac4ce3aa7a9a5b4
Adri-afk28 <adrianrecuenco@gmail.com>
2024-07-23 13:40:00 +0200

    Create Adri-afk28.c

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c/Adri-afk28.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define VALOR_CONSTANTE 1.0   //CONSTANTE
+
+int main(){
+
+//https://www.iso.org/standard/74528.html
+//Primer tipo de comentario
+///Segundo tipo de comentario
+/*
+  Comentario compuesto
+  */
+  //VARIABLES
+  int numero;
+  char letras[20];
+  double numero grande;
+  float decimal;
+  bool prueba;
+
+  printf("¡Hola, c!");
+return 0;
+}
+ 
+  
+  
+  
+return '0';
+}
